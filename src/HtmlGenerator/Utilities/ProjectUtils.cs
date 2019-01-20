1ad84db520ff357dc8afa1a4baa23e5eb0097366
Vadym <vadimart92@gmail.com>
2019-01-20 20:46:31 +0200

    accidentally ignored file added

---
+++ src/HtmlGenerator/Utilities/ProjectUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.SourceBrowser.HtmlGenerator.Utilities
+{
+	using Microsoft.CodeAnalysis;
+
+	public static class ProjectUtils
+	{
+		public static string GetAssemblyName(this Project project) {
+			return project.Name.EndsWith("(netstandard2.0)", StringComparison.OrdinalIgnoreCase)
+				? project.Name
+				: project.AssemblyName;
+		}
+	}
+}
